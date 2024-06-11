bccb7c45ac7cc5ab5d7deed7b15a78e6875508b8
Guilerme Barciki <guilerme.barciki@vissimo.com.br>
2024-06-11 20:45:43 -0300

    setup viewCode

---
+++ CoraChallenge/Scenes/Home/Views/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  CoraChallenge
+//
+//  Created by Guilerme Barciki on 11/06/24.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
