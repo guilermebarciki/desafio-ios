6ff805b314f767ca8f600132d1b11e67166438f2
Guilerme Barciki <guilerme.barciki@vissimo.com.br>
2024-06-12 16:13:40 -0300

    homeViewController initial implementation

--- CoraChallenge/Scenes/Home/Views/HomeViewController.swift
+++ CoraChallenge/Scenes/Home/Views/HomeViewController.swift
@@ -9,8 +9,123 @@ import UIKit
 
 final class HomeViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    private lazy var logoImage: UIImageView = {
+        let imageView = UIImageView()
+        #warning("remove color")
+        imageView.backgroundColor = .green
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var homeImage: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "home-image"))
+        #warning("remove color")
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Conta Digital PJ\nPoderosamente simples"
+        label.numberOfLines = 0
+    #warning("make style")
+        return label
+    }()
+    
+//    private lazy var subTitleLabel: UILabel = {
+//        let label = UILabel()
+//    #warning("make style")
+//        return label
+//    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sua empresa livre burocracias e de taxas para gerar boletos, fazer transferências e pagamentos."
+        label.numberOfLines = 0
+    #warning("make style")
+        return label
+    }()
+    
+    private lazy var textStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, descriptionLabel])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        stackView.backgroundColor = .green
+        return stackView
+    }()
+    
+    private lazy var signUpButton: CoraButton = {
+        //botaozao
+        let button = CoraButton()
+        return button
+    }()
+    
+    private lazy var signInButton: CoraButton = {
+        // ja sou cliente
+        let button = CoraButton()
+        return button
+    }()
+    
+    private lazy var buttonStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [signUpButton, signInButton])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        return stackView
+    }()
+    
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setupInterface()
+        setupConstraints()
     }
+    
+    // MARK: - View Setup
+    
+    
+    private func setupInterface() {
+        view.backgroundColor = .systemPink
+        view.addSubview(homeImage)
+        view.addSubview(logoImage)
+        view.addSubview(textStackView)
+        view.addSubview(buttonStackView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            homeImage.topAnchor.constraint(equalTo: view.topAnchor),
+            homeImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            homeImage.bottomAnchor.constraint(equalTo: textStackView.topAnchor, constant: -16)
+        ])
+        
+        NSLayoutConstraint.activate([
+            logoImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                           constant: 24),
+            logoImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24)
+        ])
+        
+        NSLayoutConstraint.activate([
+            textStackView.topAnchor.constraint(equalTo: view.centerYAnchor),
+            textStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            textStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+        ])
+        
+        NSLayoutConstraint.activate([
+            buttonStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            buttonStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            buttonStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16)
+        ])
+        
+    }
+    
+    // MARK: - Private Methods
 }
---
+++ CoraChallenge/UIComponents/CoraButton.swift
@@ -0,0 +1,21 @@
+//
+//  CoraButton.swift
+//  CoraChallenge
+//
+//  Created by Guilerme Barciki on 11/06/24.
+//
+
+import UIKit
+
+final class CoraButton: UIButton {
+    
+    init() {
+        super.init(frame: .zero)
+        backgroundColor = .gray
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
